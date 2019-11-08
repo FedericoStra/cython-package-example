c981b8dca5c02b8e9dc48e8d368d4d419a8af2f9
Federico Stra <stra.federico@gmail.com>
2019-11-08 16:19:23 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ with open("README.md") as fp:
 
 setup(
     name="cython-package-example",
-    version="0.1.0",
+    version="0.1.1",
     description="Example of a package with Cython extensions",
     long_description=long_description,
     long_description_content_type="text/markdown",
